79d45866fd68327cfcd971f5b4752b8fb9f3bc1c
oliber <oliber.garcia@gmail.com>
2019-04-15 07:34:15 -0300

    El front

--- frontend/entrevista/src/app/app.module.ts
+++ frontend/entrevista/src/app/app.module.ts
@@ -1,14 +1,22 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import {ReactiveFormsModule} from "@angular/forms";
 
 import { AppComponent } from './app.component';
+import { PedidosComponent } from './comp/pedidos/pedidos.component';
+import { HttpClientModule } from '@angular/common/http';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    PedidosComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    HttpClientModule,
+    FormsModule,
+    ReactiveFormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
